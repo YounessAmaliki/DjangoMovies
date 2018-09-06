e019d6af96e41e5ae4384b992ca1cbf3684bf7d5
Amaliki Youness <youness.amaliki@student.ap.be>
2018-09-06 16:56:45 +0200

    Project afgesloten

--- mysite/movies/urls.py
+++ mysite/movies/urls.py
@@ -4,4 +4,6 @@ from movies import views
 urlpatterns = [
    
     url(r'^test/$', views.show, name='test'),
+    url(r'^', views.actors, name='actors'),
+    url(r'^(?P<movie>[a-Z]+)/actors/$', views.results, name='results'),
 ]
--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -4,11 +4,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from urllib.parse import quote  
+from urllib.request import urlopen
 import redis
+import os.path
+import json
+
 # Create your views here.
 
 #sadd answers "It is certain" "It is decidedly so" "Without a doubt" "Yes definitely" "You may rely on it" "As I see it, yes" "Most likely" "Outlook good" "Yes" "Signs point to yes" "Reply hazy try again" "Ask again later" "Better not tell you now" "Cannot predict now" "Concentrate and ask again" "Don't count on it" "My reply is no" "My sources say no" "Outlook not so good" "Very doubtful"
 r = redis.StrictRedis(host='localhost', port=6379, db = 0);
+
+def fill_db():
+    r.set('The Godfather', '{"Authors": "Al Pacino", "Marlon Brando", "Robert Duvall"}', 'Schindlers List', '{"Authors": "Liam Neeson", "Ralph Fennes", "Ben Kingsley"}', 'Saving Private', '{"Authors": "Tom Hanks", "Matt Damon", "Vin Diesel"}')
 
 
 def show(request):
@@ -17,5 +25,16 @@ def show(request):
     f.close();
     context = {'file_content': file_content};
     return render(request, "test.html", context);
+
+
+#def results(request, movie):
+ 	#movielist = {}
+    #movie = movie.lower()
+    #if (r.get(movie) is None):
+        #response = quote(movie)
+        #obj = response.read().decode("utf8")
+        #movie = json.loads(obj)
+        #movie = json.loads(r.get(movie['Authors']))
+   # return render(request, 'authors.html', {'authors': authors})
 
 
